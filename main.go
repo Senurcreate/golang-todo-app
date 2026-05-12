041e26ce8fabdef391597b3e8156e85d8bee6f49
Senurcreate <shr1212ni@gmail.com>
2026-05-12 07:41:36 +0530

    Final Commit

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var shortGolang = "Watch Go crash course"
+var fullGolang = "Watch Nana's Golang Full Course"
+var rewardDessert = "Reward myself with a donut"
+var taskItems = []string{shortGolang, fullGolang, rewardDessert}
 
 func main() {
-	fmt.Println("Welcome toDoList App!")
+	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTasks)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+	for _, task := range taskItems {
+		fmt.Fprintln(writer, task)
+	}
+}
+
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Hello user. Welcome to our Todolist App!"
+	fmt.Fprintln(writer, greeting)
 }
